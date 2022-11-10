30266a2eb8cfeb077a98364455334534b60a6479
ZhangSan <31501054+YeMaotar@users.noreply.github.com>
2022-11-10 14:32:51 +0800

    新增-采购平台后台任务

---
+++ xemc-quartz/src/main/java/com/yunyou/xemcquartz/job/ecg/EcgMaterialJob.java
@@ -0,0 +1,17 @@
+package com.yunyou.xemcquartz.job.ecg;
+
+import cn.hutool.core.date.DateUtil;
+import com.yunyou.xemcquartz.job.base.BaseAutoPluginWork;
+import com.yunyou.xemcquartz.job.base.BaseJob;
+import lombok.extern.slf4j.Slf4j;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+
+@Slf4j
+public class EcgMaterialJob extends BaseAutoPluginWork implements BaseJob {
+    @Override
+    public void execute(JobExecutionContext context) throws JobExecutionException {
+        String flag = super.service.EcgMaterialService();
+        log.warn("采购平台物料档案 Job 执行时间: {}", DateUtil.now()+" "+flag);
+    }
+}
